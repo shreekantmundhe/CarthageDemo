57bff4dc0da5bdb4be1e085c5afd11fef496c92f
Shrikant Mundhe <Shrikant.Mundhe@derivco.co.uk>
2018-02-28 12:08:58 +0000

    Initial Commit

---
+++ Demo/Demo.h
@@ -0,0 +1,19 @@
+//
+//  Demo.h
+//  Demo
+//
+//  Created by Shrikant Mundhe on 28/02/2018.
+//  Copyright © 2018 Shrikant Mundhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Demo.
+FOUNDATION_EXPORT double DemoVersionNumber;
+
+//! Project version string for Demo.
+FOUNDATION_EXPORT const unsigned char DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo/PublicHeader.h>
+
+
