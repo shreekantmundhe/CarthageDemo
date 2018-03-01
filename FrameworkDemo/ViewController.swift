094ab3c76144c1343cf2f4cef08250f075363125
Shrikant Mundhe <Shrikant.Mundhe@derivco.co.uk>
2018-03-01 11:05:26 +0000

    Travis configuration

---
+++ FrameworkDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FrameworkDemo
+//
+//  Created by Shrikant Mundhe on 28/02/2018.
+//  Copyright © 2018 Shrikant Mundhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
